2569b3edca1a64d6ea949916298e6f64dd27fe16
Omar Hassan <66578398+S-tuberosum@users.noreply.github.com>
2023-04-10 14:34:56 -0400

    Update clean.py

--- clean.py
+++ clean.py
@@ -1,13 +1,8 @@
 import numpy as np
 import torch
 import uproot
-import awkward as ak
-import sys
-import matplotlib.pyplot as plt
-import torch.nn as nn
-import itertools
 
-file = uproot.open("ROOT files/pi+_5GeV_20deg_22deg_1e3.edm4hep.root")['events']
+file = uproot.open("ROOT files/pi+_5GeV_20deg_22deg_1e4.edm4hep.root")['events']
 branches = file.arrays()
 
 # gathering all raw data
@@ -18,22 +13,19 @@ y_raw = branches['DRICHHits.position.y'].tolist()
 x = [x for x in x_raw if x]
 y = [y for y in y_raw if y]
 
-# data array with format [particle type, energy, xy positions, x momentum, y momentum]
-
-fin_x = []
-fin_y = []
+xy = []
 
 for i in range(len(x)):
-
     # removing non circles
     if len(x[i]) < 50 or len(x[i]) > 500:
         continue
-    
+
+    # coordinates and target
+    xy.append((np.stack((x[i], y[i]), axis=-1),0))
+
     # top right
-    if min(y[i]) > 600 and max(y[i]) < 1800 and min(x[i]) > 200 and max(x[i]) < 1400: 
-        fin_x.extend(x[i])
-        fin_y.extend(y[i])
+    # if min(y[i]) > 600 and max(y[i]) < 1900 and min(x[i]) > 200 and max(x[i]) < 1500: 
+    #     xy.append((np.stack((x[i], y[i]), axis=-1),0))
 
-torch.save(fin_x, 'fin_x.pt') 
-torch.save(fin_y, 'fin_y.pt') 
-
+print(len(xy))
+torch.save(xy, 'pion.pt') 
